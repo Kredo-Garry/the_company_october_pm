b90e2d5eae4869a9629925197d7faf9b40a66e5e
Kredo-Garry <kredoteacher.grodriguez@gmail.com>
2022-10-12 20:11:49 +0800

    third upload with changes on register page

--- Views/Register.php
+++ Views/Register.php
@@ -41,6 +41,7 @@
                     <button type="submit" class="btn btn-success w-100">Register</button>
                 </form>
                 <p class="text-center small mt-3">Already registered? <a href="../Views/index.php">Login</a></p>
+                <p>This is the register page.</p>
             </div>
         </div>
     </div>
